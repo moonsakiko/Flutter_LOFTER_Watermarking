9f9a8357fed950714183ca30a7e6bd8f4e7cd5bf
moonsakiko <1912690051@qq.com>
2026-01-10 13:10:53 +0800

    Update via Termux

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart'; // 👈 引入这个
 import 'screens/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  
+  @override
+  void initState() {
+    super.initState();
+    _clearCache();
+  }
+
+  // 👇 启动时清理缓存，解决相册重复图片问题
+  Future<void> _clearCache() async {
+    try {
+      await FilePicker.platform.clearTemporaryFiles();
+      print("🧹 缓存已清理");
+    } catch (e) {
+      print("缓存清理失败: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'LOFTER Fixer',
+      title: 'LOFTER去水印',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
